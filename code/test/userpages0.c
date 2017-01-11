546fd6419537d2f73944c3e4183cb1c29db5f897
sklins <sonnik_007@mail.ru>
2017-01-11 11:13:40 +0100

    added userpages0 test file for stage 4, test fails cuse of stage 3 has a bug

---
+++ code/test/userpages0.c
@@ -0,0 +1,24 @@
+#include "syscall.h"
+
+void a(void *i) {
+    int f;
+    for(f=0; f<3;f++) {
+      PutChar('a');
+    }
+    UserThreadExit();
+}
+
+void b(void *i) {
+    int g;
+    for(g=0; g<3;g++) {
+      PutChar('b');
+    }
+    UserThreadExit();
+}
+
+int main() {
+
+  UserThreadCreate(a, (void*)5);
+  UserThreadCreate(b, (void*)7);
+  return 0 ;
+}
